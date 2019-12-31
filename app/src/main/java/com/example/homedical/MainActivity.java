7b2581babd40ca257b313c60e14b9fa9b5fe7d38
Amit Znaft <AmitZnaft@Ors-MacBook-Pro-3.local>
2019-12-31 13:40:38 +0200

    update

---
+++ app/src/main/java/com/example/homedical/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.homedical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = fAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+
+        } else {
+           startActivity(new Intent(MainActivity.this , Login.class));
+        }
+    }
+
+}
