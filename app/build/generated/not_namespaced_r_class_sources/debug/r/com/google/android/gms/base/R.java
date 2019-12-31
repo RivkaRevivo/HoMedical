e29ca4f4cbf0ded1cd839b0880327d7141dc9b95
AmitZnaft <znaft.amit@gmail.com>
2019-12-31 18:04:46 +0200

    new version

--- app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/gms/base/R.java
@@ -65,12 +65,12 @@ public final class R {
         public static final int adjust_height = 0x7f09003b;
         public static final int adjust_width = 0x7f09003c;
         public static final int auto = 0x7f090040;
-        public static final int dark = 0x7f090050;
-        public static final int icon_only = 0x7f090065;
-        public static final int light = 0x7f09006c;
-        public static final int none = 0x7f090076;
-        public static final int standard = 0x7f0900a1;
-        public static final int wide = 0x7f0900c0;
+        public static final int dark = 0x7f090052;
+        public static final int icon_only = 0x7f090069;
+        public static final int light = 0x7f090070;
+        public static final int none = 0x7f09007a;
+        public static final int standard = 0x7f0900a5;
+        public static final int wide = 0x7f0900c6;
     }
     public static final class string {
         private string() {}
--- app/src/main/java/com/example/homedical/MainActivity.java
+++ app/src/main/java/com/example/homedical/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,12 +13,29 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth fAuth;
+    Button head;
+    Button belly;
+    Button back;
+    Button thorat;
+    Button eyes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fAuth = FirebaseAuth.getInstance();
+        head = (Button)findViewById(R.id.head);
+        belly = (Button)findViewById(R.id.belly);
+        back = (Button)findViewById(R.id.back);
+        thorat = (Button)findViewById(R.id.t);
+        eyes = (Button)findViewById(R.id.eyes);
+//        head.setOnClickListener((View.OnClickListener) this);
+//        belly.setOnClickListener((View.OnClickListener) this);
+//        back.setOnClickListener((View.OnClickListener) this);
+//        thorat.setOnClickListener((View.OnClickListener) this);
+//        eyes.setOnClickListener((View.OnClickListener) this);
+
+
     }
 
     @Override
