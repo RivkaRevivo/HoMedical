78cf4df37a0da9aed5ecafb06aacf1b954a9aaeb
rivka <rivkarevivo1997@gmail.com>
2020-01-07 11:59:28 +0200

    1.2

--- app/src/main/java/com/example/homedical/MainActivity.java
+++ app/src/main/java/com/example/homedical/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.homedical;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -62,6 +66,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            //case R.id.personal
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
